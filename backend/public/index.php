0414cd9e6ae441ae4b03a6cabf2f1a89005b1aa6
dpopadiuk25 <dpopadiuk25@gmail.com>
2026-01-23 22:36:49 +0100

    konfiguracja-połączenia-i-cors

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,6 +1,14 @@
 <?php
+session_start();
 // Wstępna konfiguracja CORS
-header("Access-Control-Allow-Origin: *");
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+if ($origin) {
+    header("Access-Control-Allow-Origin: $origin");
+    header('Vary: Origin');
+    header('Access-Control-Allow-Credentials: true');
+} else {
+    header("Access-Control-Allow-Origin: *");
+}
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
